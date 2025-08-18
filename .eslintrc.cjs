58aad68864b64ce51876015b20b69e57a3ac2374
Grobmak <grobmak@email.com>
2025-08-18 08:46:02 +0700

    chore: lint fixed + config stabilized

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+﻿module.exports = {
+  env: { node: true, es2021: true },
+  extends: ["standard","prettier"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  rules: {
+    "no-irregular-whitespace": "off",
+    "no-case-declarations": "off",
+    "promise/param-names": "off",
+    "camelcase": "off",
+    "no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }],
+    "no-console": "off"
+  }
+};
